7379f484fbbabbf697658b83ea8e18658df1aacb
maksasj <radioboos@gmail.com>
2023-08-20 19:30:18 +0300

    Integrated omni types library

--- src/bebone/core/core.h
+++ src/bebone/core/core.h
@@ -1,6 +1,8 @@
 #ifndef _BEBONE_CORE_H_
 #define _BEBONE_CORE_H_
 
+#include "types.h"
+
 #include "logger.tpp"
 
 #include "arena_allocator.h"
---
+++ src/bebone/core/types.h
@@ -0,0 +1,16 @@
+#ifndef _BEBONE_CORE_TYPES_H_
+#define _BEBONE_CORE_TYPES_H_
+
+#include <omni_types.tpp>
+
+namespace bebone::core {
+    using Vec2f = omni::types::Vec2f;
+    using Vec3f = omni::types::Vec3f;
+    using Vec4f = omni::types::Vec4f;
+
+    using Vec2i = omni::types::Vec2i;
+    using Vec3i = omni::types::Vec3i;
+    using Vec4i = omni::types::Vec4i;
+}
+
+#endif
