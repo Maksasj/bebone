a1e4edd979671c1ca3bdec509b99685888c24b91
maksasj <radioboos@gmail.com>
2023-10-01 15:35:37 +0300

    deleted unused make and test bash files

--- make.bat
+++
@@ -1,8 +0,0 @@
-cmake --build build
-
-:: build\src\client\Client.exe
-
-build\examples\gfx\vulkan\0_vulkan_window\Bebone_Example_Gfx_0_Vulkan_Window
-build\examples\gfx\vulkan\1_vulkan_hello_triangle\Bebone_Example_Gfx_1_Vulkan_Hello_Triangle
-build\examples\gfx\vulkan\2_vulkan_3d_cube\Bebone_Example_Gfx_2_Vulkan_3D_Cube
-build\examples\gfx\vulkan\3_vulkan_voxel\Bebone_Example_Gfx_3_Vulkan_Voxel
