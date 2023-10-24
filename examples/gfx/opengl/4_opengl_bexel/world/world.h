9d91bd2b84b53f4ed5cda874a0c8d0a8c8cdc128
maksasj <radioboos@gmail.com>
2023-10-24 19:10:12 +0300

    removed unnecessary std::hash specialization

--- examples/gfx/opengl/4_opengl_bexel/world/world.h
+++ examples/gfx/opengl/4_opengl_bexel/world/world.h
@@ -9,15 +9,6 @@
 #include "renderable.h"
 #include "camera.h"
 
-namespace std {
-    template <>
-    struct hash<omni::types::Vec2i> {
-        size_t operator()(const omni::types::Vec2i& key) const {
-            return std::hash<int>()(key.x) ^ std::hash<int>()(key.y);
-        }
-    };
-}
-
 namespace bexel {
     class World final : public Renderable, private core::NonCopyable {
         private:
