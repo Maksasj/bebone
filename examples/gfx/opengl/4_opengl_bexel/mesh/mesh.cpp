b5e0da413ca9054d988a89b7afcd25d04d9e6af2
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-10-24 19:56:32 +0300

    bexel fixed

--- examples/gfx/opengl/4_opengl_bexel/mesh/mesh.cpp
+++ examples/gfx/opengl/4_opengl_bexel/mesh/mesh.cpp
@@ -5,8 +5,8 @@ namespace bexel {
         m_vao = make_unique<GLVertexArrayObject>();
         m_vao->bind();
 
-        m_vbo = make_unique<GLVertexBufferObject>(m_vertices.data(), m_vertices.size() * sizeof(Vertex));
-        m_ebo = make_unique<GLElementBufferObject>(m_indices);
+        m_vbo = make_unique<GLVertexBufferObject>(m_vertices.data(), m_vertices.size() * sizeof(Vertex), GL_STATIC_DRAW);
+        m_ebo = make_unique<GLElementBufferObject>(m_indices.data(), m_indices.size(), GL_STATIC_DRAW);
 
         m_vao->link_attributes(*m_vbo, 0, 3, GL_FLOAT, sizeof(Vertex), (void*) offsetof(Vertex, pos));
         m_vao->link_attributes(*m_vbo, 1, 2, GL_FLOAT, sizeof(Vertex), (void*) offsetof(Vertex, tex));
