06d0b446125a8545596ffeb86045bf79454e14aa
maksasj <radioboos@gmail.com>
2023-12-18 17:26:54 +0200

    configured imgui with cmake

--- src/bebone/gfx/begui/begui.h
+++ src/bebone/gfx/begui/begui.h
@@ -1,9 +1,8 @@
 #ifndef _BEBONE_BEBONE_IMGUI_H_
 #define _BEBONE_BEBONE_IMGUI_H_
 
-// Todo resolve this cringe
-#include "../../../3dparty/imgui/imgui.h"
-#include "../../../3dparty/imgui/backends/imgui_impl_glfw.h"
-#include "../../../3dparty/imgui/backends/imgui_impl_opengl3.h"
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #endif
