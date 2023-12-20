f18288eb41c858ac6a8021af6a49174da8cf290a
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-12-20 19:45:28 +0200

    VSync on

--- examples/gfx/opengl/5_opengl_flappy_bird/main.cpp
+++ examples/gfx/opengl/5_opengl_flappy_bird/main.cpp
@@ -32,6 +32,7 @@ int main() {
 
     GLContext::load_opengl();
     GLContext::set_viewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glfwSwapInterval(1);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
