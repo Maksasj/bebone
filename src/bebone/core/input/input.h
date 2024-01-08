e15dbcd199bfe4e13966504ee57c5455405b3a1e
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-01-08 11:04:40 +0200

    key struct, input implementation

--- src/bebone/core/input/input.h
+++ src/bebone/core/input/input.h
@@ -8,6 +8,7 @@
 
 #include "input_type.h"
 #include "key_code.h"
+#include "key.h"
 
 namespace bebone::core {
     using namespace std;
@@ -16,14 +17,23 @@ namespace bebone::core {
         using Action = Action<>;
 
     private:
-        map<KeyCode, Action> keyActions;
-        queue<KeyCode> queuedKeys;
+        map<Key, Action> keyActions;
+        queue<Key> queuedKeys;
 
+        Input() {}
+
+        void queue_key(const KeyCode& keyCode, const InputType& inputType);
+        void execute_queued_actions();
+
+        // TODO: friend class (for example, window will queue the keys using callbacks and will execute queued keys before draw calls)
     public:
+        Input(const Input& obj) = delete;
+        void operator=(Input const& obj) = delete;
+
+        static Input& get_instance();
+
         void register_key_action(const KeyCode& keyCode, std::function<void()>& action, const InputType& inputType = InputType::PRESS);
         void remove_key_action(const KeyCode& keyCode, std::function<void()>& action, const InputType& inputType = InputType::PRESS);
-        void send_key_to_the_queue(const KeyCode& keyCode);
-        void execute_pooled_actions();
     };
 }
 
