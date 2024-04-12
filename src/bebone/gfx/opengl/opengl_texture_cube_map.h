f6894a841cb724b492e41808390cea99e708ffa9
maksasj <radioboos@gmail.com>
2024-04-12 19:40:55 +0300

    test cube map implementation

--- src/bebone/gfx/opengl/opengl_texture_cube_map.h
+++ src/bebone/gfx/opengl/opengl_texture_cube_map.h
@@ -12,19 +12,48 @@ namespace bebone::gfx::opengl {
 
     class GLTextureCubeMap : public GLTexture {
         private:
+            template<typename _Color>
+            void create_cube_map_texture(const std::array<std::shared_ptr<Image<_Color>>, 6>& images) {
+                bind();
+
+                for(auto i = 0; i < 6; ++i) {
+                    const auto width = images[i]->get_width();
+                    const auto height = images[i]->get_height();
+
+                    const auto format = ColorRGBA::get_gl_format();
+                    const auto type = ColorRGBA::get_gl_type();
+
+                    glTexImage2D(
+                        GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                        0, format, width, height, 0,
+                        format, type, images[i]->data());
+                }
+
+                const GLTextureParameters parameters;
+                configure_gl_texture(parameters);
+                generate_mipmap();
+
+                unbind();
+            }
 
         public:
+            GLTextureCubeMap(const std::vector<std::string> filePaths) : GLTexture(GL_TEXTURE_CUBE_MAP) {
+                std::array<std::shared_ptr<Image<ColorRGBA>>, 6> images = {
+                    Image<ColorRGBA>::load_from_file(filePaths[0]),
+                    Image<ColorRGBA>::load_from_file(filePaths[1]),
+                    Image<ColorRGBA>::load_from_file(filePaths[2]),
+                    Image<ColorRGBA>::load_from_file(filePaths[3]),
+                    Image<ColorRGBA>::load_from_file(filePaths[4]),
+                    Image<ColorRGBA>::load_from_file(filePaths[5]),
+                };
 
-            /*
-            GLTexture2D(const std::string& filePath) : GLTexture(filePath, GL_TEXTURE_2D) {
-
+                create_cube_map_texture(images);
             }
 
             template<typename _Color>
-            GLTexture2D(const std::shared_ptr<Image<_Color>>& image) : GLTexture(image, GL_TEXTURE_2D) {
-
+            GLTextureCubeMap(const std::array<std::shared_ptr<Image<_Color>>, 6>& images) : GLTexture(GL_TEXTURE_CUBE_MAP) {
+                create_cube_map_texture(images);
             }
-            */
     };
 }
 
