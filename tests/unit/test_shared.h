b0bd05d26409558351de876655f16fd262f88203
Maksim Jarolsavcevas <radioboos@gmail.com>
2024-04-30 12:47:56 +0300

    removed tests to bebone_tests repo

--- tests/unit/test_shared.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BEBONE_TESTING_TEST_SHARED_H_
-#define _BEBONE_TESTING_TEST_SHARED_H_
-
-#define ensure(EXP) if(!(EXP)) return 1;
-#define ensure_const(EXP) if constexpr (!(EXP)) return 1;
-#define TEST_CASE
-
-#endif
