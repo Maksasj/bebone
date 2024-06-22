70b7a4415316e9aa3500ba0465c88019c96dd924
maksasj <radioboos@gmail.com>
2024-06-22 20:30:41 +0300

    vulkan image memory tuple

--- src/bebone/gfx/vulkan/vulkan_texture.cpp
+++ src/bebone/gfx/vulkan/vulkan_texture.cpp
@@ -12,17 +12,12 @@ namespace bebone::gfx::vulkan {
         std::shared_ptr<VulkanCommandBufferPool>& commandBufferPool,
         const std::shared_ptr<Image<ColorRGBA>>& raw
     ) {
-        // Todo make create_image_memory function to device
-        image = device.create_image(
-            VK_FORMAT_R32G32B32A32_SFLOAT,
+        auto [im, mem] = device.create_image_memory(VK_FORMAT_R32G32B32A32_SFLOAT,
             { static_cast<uint32_t>(raw->get_width()), static_cast<uint32_t>(raw->get_height()), 1},
             { .usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT }
         );
 
-        auto memReq = image->get_memory_requirements(device);
-
-        memory = device.create_device_memory(memReq, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-        memory->bind_image_memory(device, image);
+        image = im; memory = mem;
 
         image->transition_layout(*commandBufferPool, device, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
 
