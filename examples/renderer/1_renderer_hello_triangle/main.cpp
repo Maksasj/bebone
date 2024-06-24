d5c20e234684e6639d376c8cd9d9f59241c3ddab
maksasj <radioboos@gmail.com>
2024-06-24 16:39:26 +0300

    base for renderer window, hello triangle and sprite examples

---
+++ examples/renderer/1_renderer_hello_triangle/main.cpp
@@ -0,0 +1,24 @@
+#include "bebone/bebone.h"
+
+using namespace bebone::renderer;
+using namespace bebone::gfx;
+
+int main() {
+    /*
+    GLFWContext::init(); // this should be moved somewhere else
+
+    auto window = WindowFactory::create_window("0. OpenGL window example", 800, 600, GfxAPI::OPENGL);
+    auto renderer = window->create_renderer();
+
+    auto sprite = renderer.load_sprite("image.png");
+
+    while(!window->closing()) {
+        renderer->renderer(sprite, { .pos = Vec3::zero });
+        renderer->present();
+    }
+
+    GLFWContext::terminate();// this should be moved somewhere else
+    */
+
+    return 0;
+}
