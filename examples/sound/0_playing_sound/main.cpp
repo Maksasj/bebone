42a9af4315a11439e318054d08fe27594c274e86
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-07-04 16:49:42 +0300

    Added sound example

---
+++ examples/sound/0_playing_sound/main.cpp
@@ -0,0 +1,15 @@
+#include "bebone/bebone.h"
+
+using namespace bebone::sound_system;
+
+int main() {
+    SoundEngine sound_engine;
+    //Sound coin_sound = sound_engine.load_sound("coin_sound.mp3");
+
+    //coin_sound.play();
+
+    //std::cout << "Press any key to exit...";
+    //getchar();
+
+    return 0;
+}
