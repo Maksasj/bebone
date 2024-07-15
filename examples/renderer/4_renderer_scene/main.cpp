d023bc8d6422f9ee2db49860676d4d3804b2db2e
maksasj <radioboos@gmail.com>
2024-07-15 17:35:11 +0300

    base for 4th example

---
+++ examples/renderer/4_renderer_scene/main.cpp
@@ -0,0 +1,25 @@
+#include "bebone/bebone.h"
+
+using namespace bebone::renderer;
+using namespace bebone::gfx;
+
+int main() {
+    GLFWContext::init(); // this should be moved somewhere else
+
+    auto window = WindowFactory::create_window("4. Renderer scene", 800, 600, Vulkan, { .enable_resize = true });
+    auto renderer = RendererFactory::create_renderer(window);
+
+    auto suzanne = renderer->load_mesh("suzanne.obj");
+
+    while(!window->closing()) {
+        renderer->render(suzanne, { .position = Vec3f::zero });
+        renderer->present();
+
+        window->pull_events();
+    }
+
+    GLFWContext::terminate();// this should be moved somewhere else
+
+    return 0;
+}
+
