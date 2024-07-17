7b88f4a6f24e1612f77e63da148ce240cbfca3d7
maksasj <radioboos@gmail.com>
2024-07-17 14:46:06 +0300

    little fix

--- examples/gfx/vulkan/2_vulkan_texture/frag.glsl
+++ examples/gfx/vulkan/2_vulkan_texture/frag.glsl
@@ -5,7 +5,7 @@ layout (location = 1) in vec2 tex_cord;
 
 layout (location = 0) out vec4 out_color;
 
-layout(binding = 2) uniform sampler2D textures[];
+layout(set = 0, binding = 2) uniform sampler2D textures[];
 
 void main() {
    out_color = vec4(texture(textures[0], tex_cord).rgb, 1.0);
