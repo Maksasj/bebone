9f9e77c2331ab98cdaf303de5e16eb65a54b70bd
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-07-21 07:23:16 +0300

    Added look_at

--- src/bebone/core/types/matrix/matrix_4x4.h
+++ src/bebone/core/types/matrix/matrix_4x4.h
@@ -42,6 +42,7 @@ namespace bebone::core {
         static Matrix<f32, 4, 4> orthographic(const f32& right, const f32& left, const f32& bottom, const f32& top, const f32& near, const f32& far);
         static Matrix<f32, 4, 4> perspective(f32 fov, const f32& aspect, const f32& near, const f32& far);
         static Matrix<f32, 4, 4> view(const Vec3f& origin, const Vec3f& direction, const Vec3f& up = Vec3f::up);
+        static Matrix<f32, 4, 4> look_at(const Vec3f origin, const Vec3f center, const Vec3f& up = Vec3f::up);
 
         inline Matrix<f32, 4, 4> transpose() const;
         inline Matrix<f32, 4, 4> inverse() const;
