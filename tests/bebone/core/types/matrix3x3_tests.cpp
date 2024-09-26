44ab192e622257464bb964a547012d3db25bea9e
Maksim Jaroslavcevas <36482401+Maksasj@users.noreply.github.com>
2024-09-26 19:24:50 +0300

    refactored matrix3x3 tests

--- tests/bebone/core/types/matrix3x3_tests.cpp
+++ tests/bebone/core/types/matrix3x3_tests.cpp
@@ -4,10 +4,17 @@
 using namespace bebone::core;
 
 TEST(Matrix3x3Math, Initialization) {
+    // Arrange
     Matrix3x3f a(4, 3, 1,
                  2, 1, 0,
                  5, 3, 2);
 
+    Matrix3x3f b(5, 6, 3,
+                 7, 8, 2,
+                 6, 3, 1);
+    // Act
+
+    // Assert
     ASSERT_EQ(a(0, 0), 4);
     ASSERT_EQ(a(0, 1), 3);
     ASSERT_EQ(a(0, 2), 1);
@@ -17,10 +24,6 @@ TEST(Matrix3x3Math, Initialization) {
     ASSERT_EQ(a(2, 0), 5);
     ASSERT_EQ(a(2, 1), 3);
     ASSERT_EQ(a(2, 2), 2);
-
-    Matrix3x3f b(5, 6, 3,
-                 7, 8, 2,
-                 6, 3, 1);
 
     ASSERT_EQ(b(0, 0), 5);
     ASSERT_EQ(b(0, 1), 6);
@@ -34,6 +37,7 @@ TEST(Matrix3x3Math, Initialization) {
 }
 
 TEST(Matrix3x3Math, MatrixAddition) {
+    // Arrange
     Matrix3x3f a(1, 2, 3,
                  4, 5, 6,
                  7, 8, 9);
@@ -46,17 +50,22 @@ TEST(Matrix3x3Math, MatrixAddition) {
                  5, 4, 3,
                  6, 0, 0);
 
-    Matrix3x3f result = a + b;
-    ASSERT_TRUE(result == Matrix3x3f(4, 6, 4,
+    // Act
+    Matrix3x3f result0 = a + b;
+    Matrix3x3f result1 = b + c;
+
+    // Assert
+    ASSERT_TRUE(result0 == Matrix3x3f(4, 6, 4,
                                      9, 11, 6,
                                      11, 10, 10));
-    result = b + c;
-    ASSERT_TRUE(result == Matrix3x3f(6, 5, 5,
+
+    ASSERT_TRUE(result1 == Matrix3x3f(6, 5, 5,
                                      10, 10, 3,
                                      10, 2, 1));
 }
 
 TEST(Matrix3x3Math, MatrixSubtraction) {
+    // Arrange
     Matrix3x3f a(1, 2, 3,
                  4, 5, 6,
                  7, 8, 9);
@@ -69,17 +78,22 @@ TEST(Matrix3x3Math, MatrixSubtraction) {
                  5, 4, 3,
                  6, 0, 0);
 
-    Matrix3x3f result = a - b;
-    ASSERT_TRUE(result == Matrix3x3f(-2, -2, 2,
+    // Act
+    Matrix3x3f result0 = a - b;
+    Matrix3x3f result1 = a - c;
+
+    // Assert
+    ASSERT_TRUE(result0 == Matrix3x3f(-2, -2, 2,
                                      -1, -1, 6,
                                      3, 6, 8));
-    result = a - c;
-    ASSERT_TRUE(result == Matrix3x3f(-2, 1, -1,
+
+    ASSERT_TRUE(result1 == Matrix3x3f(-2, 1, -1,
                                      -1, 1, 3,
                                      1, 8, 9));
 }
 
 TEST(Matrix3x3Math, MatrixMultiplication) {
+    // Arrange
     Matrix3x3f a(1, 2, 3,
                  4, 5, 6,
                  7, 8, 9);
@@ -92,25 +106,32 @@ TEST(Matrix3x3Math, MatrixMultiplication) {
                  5, 4, 3,
                  6, 0, 0);
 
-    Matrix3x3f result = a * b;
-    ASSERT_TRUE(result == Matrix3x3f(25, 22, 4,
+    // Act
+    Matrix3x3f result0 = a * b;
+    Matrix3x3f result1 = b * a;
+    Matrix3x3f result2 = b * c;
+    Matrix3x3f result3 = c * b;
+
+    // Assert
+    ASSERT_TRUE(result0 == Matrix3x3f(25, 22, 4,
                                      61, 58, 10,
                                      97, 94, 16));
-    result = b * a;
-    ASSERT_TRUE(result == Matrix3x3f(26, 34, 42,
+
+    ASSERT_TRUE(result1 == Matrix3x3f(26, 34, 42,
                                      29, 40, 51,
                                      19, 26, 33));
-    result = b * c;
-    ASSERT_TRUE(result == Matrix3x3f(35, 19, 24,
+
+    ASSERT_TRUE(result2 == Matrix3x3f(35, 19, 24,
                                      45, 29, 38,
                                      28, 12, 22));
-    result = c * b;
-    ASSERT_TRUE(result == Matrix3x3f(30, 26, 7,
+
+    ASSERT_TRUE(result3 == Matrix3x3f(30, 26, 7,
                                      47, 50, 8,
                                      18, 24, 6));
 }
 
 TEST(Matrix3x3Math, ScalarAddition) {
+    // Arrange
     Matrix3x3f a(1, 2, 3,
                  4, 5, 6,
                  7, 8, 9);
@@ -119,17 +140,22 @@ TEST(Matrix3x3Math, ScalarAddition) {
                  5, 6, 0,
                  4, 2, 1);
 
-    Matrix3x3f result = a + 4;
-    ASSERT_TRUE(result == Matrix3x3f(5, 6, 7,
+    // Act
+    Matrix3x3f result0 = a + 4;
+    Matrix3x3f result1 = b + 7;
+
+    // Assert
+    ASSERT_TRUE(result0 == Matrix3x3f(5, 6, 7,
                                      8, 9, 10,
                                      11, 12, 13));
-    result = b + 7;
-    ASSERT_TRUE(result == Matrix3x3f(10, 11, 8,
+
+    ASSERT_TRUE(result1 == Matrix3x3f(10, 11, 8,
                                      12, 13, 7,
                                      11, 9, 8));
 }
 
 TEST(Matrix3x3Math, ScalarSubtraction) {
+    // Arrange
     Matrix3x3f a(1, 2, 3,
                  4, 5, 6,
                  7, 8, 9);
@@ -138,17 +164,22 @@ TEST(Matrix3x3Math, ScalarSubtraction) {
                  5, 6, 0,
                  4, 2, 1);
 
-    Matrix3x3f result = a - 4;
-    ASSERT_TRUE(result == Matrix3x3f(-3, -2, -1,
+    // Act
+    Matrix3x3f result0 = a - 4;
+    Matrix3x3f result1 = b - 7;
+
+    // Assert
+    ASSERT_TRUE(result0 == Matrix3x3f(-3, -2, -1,
                                      0, 1, 2,
                                      3, 4, 5));
-    result = b - 7;
-    ASSERT_TRUE(result == Matrix3x3f(-4, -3, -6,
+
+    ASSERT_TRUE(result1 == Matrix3x3f(-4, -3, -6,
                                      -2, -1, -7,
                                      -3, -5, -6));
 }
 
 TEST(Matrix3x3Math, ScalarMultiplication) {
+    // Arrange
     Matrix3x3f a(1, 2, 3,
                  4, 5, 6,
                  7, 8, 9);
@@ -157,17 +188,22 @@ TEST(Matrix3x3Math, ScalarMultiplication) {
                  5, 6, 0,
                  4, 2, 1);
 
-    Matrix3x3f result = a * 4;
-    ASSERT_TRUE(result == Matrix3x3f(4, 8, 12,
+    // Act
+    Matrix3x3f result0 = a * 4;
+    Matrix3x3f result1 = b * 7;
+
+    // Assert
+    ASSERT_TRUE(result0 == Matrix3x3f(4, 8, 12,
                                      16, 20, 24,
                                      28, 32, 36));
-    result = b * 7;
-    ASSERT_TRUE(result == Matrix3x3f(21, 28, 7,
+
+    ASSERT_TRUE(result1 == Matrix3x3f(21, 28, 7,
                                      35, 42, 0,
                                      28, 14, 7));
 }
 
 TEST(Matrix3x3Math, ScalarDivision) {
+    // Arrange
     Matrix3x3f a(25, 0, 10,
                  15, 5, 30,
                  35, 100, 45);
@@ -176,17 +212,22 @@ TEST(Matrix3x3Math, ScalarDivision) {
                  0, 8, 16,
                  20, 10, 8);
 
-    Matrix3x3f result = a / 5;
-    ASSERT_TRUE(result == Matrix3x3f(5, 0, 2,
+    // Act
+    Matrix3x3f result0 = a / 5;
+    Matrix3x3f result1 = b / 2;
+
+    // Assert
+    ASSERT_TRUE(result0 == Matrix3x3f(5, 0, 2,
                                      3, 1, 6,
                                      7, 20, 9));
-    result = b / 2;
-    ASSERT_TRUE(result == Matrix3x3f(3, 2, 1,
+
+    ASSERT_TRUE(result1 == Matrix3x3f(3, 2, 1,
                                      0, 4, 8,
                                      10, 5, 4));
 }
 
 TEST(Matrix3x3Functions, Transpose) {
+    // Arrange
     Matrix3x3f a(1, 2, 3,
                  4, 5, 6,
                  7, 8, 9);
@@ -195,17 +236,22 @@ TEST(Matrix3x3Functions, Transpose) {
                  5, 6, 0,
                  4, 2, 1);
 
-    Matrix3x3f result = a.transpose();
-    ASSERT_TRUE(result == Matrix3x3f(1, 4, 7,
+    // Act
+    Matrix3x3f result0 = a.transpose();
+    Matrix3x3f result1 = b.transpose();
+
+    // Assert
+    ASSERT_TRUE(result0 == Matrix3x3f(1, 4, 7,
                                      2, 5, 8,
                                      3, 6, 9));
-    result = b.transpose();
-    ASSERT_TRUE(result == Matrix3x3f(3, 5, 4,
+
+    ASSERT_TRUE(result1 == Matrix3x3f(3, 5, 4,
                                      4, 6, 2,
                                      1, 0, 1));
 }
 
 TEST(Matrix3x3Functions, Inverse) {
+    // Arrange
     Matrix3x3f a(5, 6, 3,
                  4, 5, 6,
                  7, 8, 9);
@@ -214,25 +260,28 @@ TEST(Matrix3x3Functions, Inverse) {
                  5, 6, 0,
                  4, 2, 1);
 
-    Matrix3x3f result = a.inverse();
-    ASSERT_FLOAT_EQ(result(0, 0), -0.25f);
-    ASSERT_FLOAT_EQ(result(0, 1), -2.5f);
-    ASSERT_FLOAT_EQ(result(0, 2), 1.75f);
-    ASSERT_FLOAT_EQ(result(1, 0), 0.5f);
-    ASSERT_FLOAT_EQ(result(1, 1), 2.0f);
-    ASSERT_FLOAT_EQ(result(1, 2), -1.5f);
-    ASSERT_FLOAT_EQ(result(2, 0), -0.25f);
-    ASSERT_FLOAT_EQ(result(2, 1), 0.1666667f);
-    ASSERT_FLOAT_EQ(result(2, 2), 0.08333333f);
+    // Act
+    Matrix3x3f result0 = a.inverse();
+    Matrix3x3f result1 = b.inverse();
 
-    result = b.inverse();
-    ASSERT_FLOAT_EQ(result(0, 0), -0.375f);
-    ASSERT_FLOAT_EQ(result(0, 1), 0.125f);
-    ASSERT_FLOAT_EQ(result(0, 2), 0.375f);
-    ASSERT_FLOAT_EQ(result(1, 0), 0.3125f);
-    ASSERT_FLOAT_EQ(result(1, 1), 0.0625f);
-    ASSERT_FLOAT_EQ(result(1, 2), -0.3125f);
-    ASSERT_FLOAT_EQ(result(2, 0), 0.875f);
-    ASSERT_FLOAT_EQ(result(2, 1), -0.625f);
-    ASSERT_FLOAT_EQ(result(2, 2), 0.125f);
+    // Assert
+    ASSERT_FLOAT_EQ(result0(0, 0), -0.25f);
+    ASSERT_FLOAT_EQ(result0(0, 1), -2.5f);
+    ASSERT_FLOAT_EQ(result0(0, 2), 1.75f);
+    ASSERT_FLOAT_EQ(result0(1, 0), 0.5f);
+    ASSERT_FLOAT_EQ(result0(1, 1), 2.0f);
+    ASSERT_FLOAT_EQ(result0(1, 2), -1.5f);
+    ASSERT_FLOAT_EQ(result0(2, 0), -0.25f);
+    ASSERT_FLOAT_EQ(result0(2, 1), 0.1666667f);
+    ASSERT_FLOAT_EQ(result0(2, 2), 0.08333333f);
+
+    ASSERT_FLOAT_EQ(result1(0, 0), -0.375f);
+    ASSERT_FLOAT_EQ(result1(0, 1), 0.125f);
+    ASSERT_FLOAT_EQ(result1(0, 2), 0.375f);
+    ASSERT_FLOAT_EQ(result1(1, 0), 0.3125f);
+    ASSERT_FLOAT_EQ(result1(1, 1), 0.0625f);
+    ASSERT_FLOAT_EQ(result1(1, 2), -0.3125f);
+    ASSERT_FLOAT_EQ(result1(2, 0), 0.875f);
+    ASSERT_FLOAT_EQ(result1(2, 1), -0.625f);
+    ASSERT_FLOAT_EQ(result1(2, 2), 0.125f);
 }
