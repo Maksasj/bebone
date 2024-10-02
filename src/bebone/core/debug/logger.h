e619901d87c033a67e65f07cc526fadd7b1c7135
Maksim Jaroslavcevas <36482401+Maksasj@users.noreply.github.com>
2024-10-02 09:47:03 +0300

    fixed mingw build

--- src/bebone/core/debug/logger.h
+++ src/bebone/core/debug/logger.h
@@ -25,13 +25,13 @@ namespace bebone::core {
     };
 }
 
-#define LOG(LOG_LEVEL, FORMAT, ...) bebone::core::Logger::log(LOG_LEVEL, FORMAT, __VA_ARGS__)
+#define LOG(LOG_LEVEL, FORMAT, ...) bebone::core::Logger::log(LOG_LEVEL, FORMAT, ##__VA_ARGS__)
 
-#define LOG_TRACE(FORMAT, ...) bebone::core::Logger::log(LogLevel::Trace, FORMAT, __VA_ARGS__)
-#define LOG_DEBUG(FORMAT, ...) bebone::core::Logger::log(LogLevel::Debug, FORMAT, __VA_ARGS__)
-#define LOG_INFORMATION(FORMAT, ...) bebone::core::Logger::log(LogLevel::Information, FORMAT, __VA_ARGS__)
-#define LOG_WARNING(FORMAT, ...) bebone::core::Logger::log(LogLevel::Warning, FORMAT, __VA_ARGS__)
-#define LOG_ERROR(FORMAT, ...) bebone::core::Logger::log(LogLevel::Error, FORMAT, __VA_ARGS__)
-#define LOG_CRITICAL(FORMAT, ...) bebone::core::Logger::log(LogLevel::Critical, FORMAT, __VA_ARGS__)
+#define LOG_TRACE(FORMAT, ...) bebone::core::Logger::log(LogLevel::Trace, FORMAT, ##__VA_ARGS__)
+#define LOG_DEBUG(FORMAT, ...) bebone::core::Logger::log(LogLevel::Debug, FORMAT, ##__VA_ARGS__)
+#define LOG_INFORMATION(FORMAT, ...) bebone::core::Logger::log(LogLevel::Information, FORMAT, ##__VA_ARGS__)
+#define LOG_WARNING(FORMAT, ...) bebone::core::Logger::log(LogLevel::Warning, FORMAT, ##__VA_ARGS__)
+#define LOG_ERROR(FORMAT, ...) bebone::core::Logger::log(LogLevel::Error, FORMAT, ##__VA_ARGS__)
+#define LOG_CRITICAL(FORMAT, ...) bebone::core::Logger::log(LogLevel::Critical, FORMAT, ##__VA_ARGS__)
 
 #endif
